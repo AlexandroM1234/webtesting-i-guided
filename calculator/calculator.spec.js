77dbb32602a947bdc2d419670cce21333ac1d578
Alex Martinez <alexmartinez0071234@gmail.com>
2020-04-22 12:56:26 -0400

    following GP

--- calculator/calculator.spec.js
+++ calculator/calculator.spec.js
@@ -1,3 +1,21 @@
-const { add } = require('./calculator.js');
+const { add } = require("./calculator.js");
 
 // test away!
+describe("calculator.js", () => {
+  // testcase
+  it("should run tests without errors", () => {
+    // arrage --> setup the world
+
+    // act --> execute the code we're testing
+
+    //assert --> check out assumptions
+    expect(true).toBeTruthy();
+  });
+  describe("add function", () => {
+    it("should return null ", () => {
+      expect(add()).toBe(null);
+    });
+  });
+});
+
+// try to make the tests fail, before making them pass
